73c5e5add6d992d844abdfc2dfc1a0b1acfaf10b
kng-w <kng.w@keio.jp>
2025-10-12 11:46:06 +0900

    update inference.py

--- inference.py
+++ inference.py
@@ -15,9 +15,9 @@ def main():
         '--model_path',
         type=str,
         default=  # noqa: E251
-        "/work/u1657859/DRCT/experiments/train_DRCT-L_SRx4_finetune_from_ImageNet_pretrain/models/DRCT-L.pth"  # noqa: E501
+        "/workspace/models/DRCT_XL.pth"  # noqa: E501
     )
-    parser.add_argument('--input', type=str, default='datasets/Set14/LRbicx4', help='input test image folder')
+    parser.add_argument('--input', type=str, default='/workspace/data/test_input', help='input test image folder')
     parser.add_argument('--output', type=str, default='results/DRCT-L', help='output folder')
     parser.add_argument('--scale', type=int, default=4, help='scale factor: 1, 2, 3, 4')
     #parser.add_argument('--window_size', type=int, default=16, help='16')
@@ -30,8 +30,9 @@ def main():
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     # set up model (DRCT-L)
     model = DRCT(upscale=4, in_chans=3,  img_size= 64, window_size= 16, compress_ratio= 3,squeeze_factor= 30,
-                        conv_scale= 0.01, overlap_ratio= 0.5, img_range= 1., depths= [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
-                        embed_dim= 180, num_heads= [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6], gc= 32,
+                        conv_scale= 0.01, overlap_ratio= 0.5, img_range= 1., depths= [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6], # L だと 6層
+                        embed_dim= 180, num_heads= [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6], # L だと 6層
+                        gc= 32,
                         mlp_ratio= 2, upsampler= 'pixelshuffle', resi_connection= '1conv')
     
     model.load_state_dict(torch.load(args.model_path)['params'], strict=True)
@@ -72,7 +73,7 @@ def main():
             output = output.data.squeeze().float().cpu().clamp_(0, 1).numpy()
             output = np.transpose(output[[2, 1, 0], :, :], (1, 2, 0))
             output = (output * 255.0).round().astype(np.uint8)
-            cv2.imwrite(os.path.join(args.output, f'{imgname}_DRCT-L_X4.png'), output)
+            cv2.imwrite(os.path.join(args.output, f'{imgname}_DRCT-L_XL.png'), output)
 
 
 def test(img_lq, model, args, window_size):
